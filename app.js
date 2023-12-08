1afd123f67e4e18f1dd69d274967e3b535ab74a7
WholeStacks <148982063+WholeStacks@users.noreply.github.com>
2023-12-08 15:47:10 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const track = document.getElementById("image-track");
+let drag = false;
+document.addEventListener('mousedown', () => drag = false);      
+document.addEventListener('mousemove', () => drag = true);
+document.addEventListener('mouseup', () => drag ? console.log('drag') : console.log('click')); //window.location=pageName);
 
-// This function registers mouse wheel as left and right instead of up and down, but doesn't trigger smooth animation for now
-// Also try to limit scroll left right in image track only
-// window.addEventListener('wheel', function(e) {
-//     if (e.deltaY != 0) {
-//         e.preventDefault();
-//         window.scroll(window.scrollX + e.deltaY, window.scrollY);
-//     }
-// }, { passive: false });
+function showTitle(titleName) {
+	document.getElementById("description").innerHTML = titleName;
+  // const bgStyle = document.body.style;
+  // switch(titleName) {
+  //   case "About me":
+  //     bgStyle.backgroundImage = "url('./assets/profile_img.jpg')";
+  //     break;
+  //   case "Skillset":
+  //     bgStyle.backgroundImage = "url('./assets/skills_img.jpg')";
+  //     break;
+  //   case "Services":
+  //     bgStyle.backgroundImage = "url('./assets/services_img.jpg')";
+  //     break;
+  //   case "Contact":
+  //     bgStyle.backgroundImage = "url('./assets/contact_img.jpg')";
+  //     break;
+  //   default:
+  //     bgStyle.backgroundImage = "url('./assets/black.jpg')";
+  //     break;
+  // }
+}
+
+function userClick(pageName) {
+	drag? null: window.location=pageName;
+}
+
+const track = document.getElementById("image-track");
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
@@ -39,7 +61,7 @@ const handleOnMove = e => {
   }
 }
 
-/* -- Had to add extra lines for touch events -- */
+/* extra lines for touch events -- */
 
 window.onmousedown = e => handleOnDown(e);
 
@@ -52,80 +74,3 @@ window.ontouchend = e => handleOnUp(e.touches[0]);
 window.onmousemove = e => handleOnMove(e);
 
 window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-//  This is for top carousel
-let loop = horizontalLoop(".image-top", {speed: 1, repeat: -1, paddingRight: 5});
-
-function setDirection(value) {
-  if (loop.direction !== value) {
-    gsap.to(loop, {timeScale: value, duration: 0.3, overwrite: true});
-    loop.direction = value;
-  }
-}
-
-// Observer.create({
-//   target: window,
-//   type: "wheel,scroll,touch",
-//   onDown: () => setDirection(1),
-//   onUp: () => setDirection(-1)
-// })
-
-
-// Helper function
-function horizontalLoop(items, config) {
-	items = gsap.utils.toArray(items);
-	config = config || {};
-	let tl = gsap.timeline({repeat: config.repeat, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)}),
-		length = items.length,
-		startX = items[0].offsetLeft,
-		times = [],
-		widths = [],
-		xPercents = [],
-		curIndex = 0,
-		pixelsPerSecond = (config.speed || 1) * 100,
-		snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
-		totalWidth, curX, distanceToStart, distanceToLoop, item, i;
-	gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-		xPercent: (i, el) => {
-			let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
-			xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
-			return xPercents[i];
-		}
-	});
-	gsap.set(items, {x: 0});
-	totalWidth = items[length-1].offsetLeft + xPercents[length-1] / 100 * widths[length-1] - startX + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0);
-	for (i = 0; i < length; i++) {
-		item = items[i];
-		curX = xPercents[i] / 100 * widths[i];
-		distanceToStart = item.offsetLeft + curX - startX;
-		distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
-		tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
-		  .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
-		  .add("label" + i, distanceToStart / pixelsPerSecond);
-		times[i] = distanceToStart / pixelsPerSecond;
-	}
-	function toIndex(index, vars) {
-		vars = vars || {};
-		(Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
-		let newIndex = gsap.utils.wrap(0, length, index),
-			time = times[newIndex];
-		if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
-			vars.modifiers = {time: gsap.utils.wrap(0, tl.duration())};
-			time += tl.duration() * (index > curIndex ? 1 : -1);
-		}
-		curIndex = newIndex;
-		vars.overwrite = true;
-		return tl.tweenTo(time, vars);
-	}
-	tl.next = vars => toIndex(curIndex+1, vars);
-	tl.previous = vars => toIndex(curIndex-1, vars);
-	tl.current = () => curIndex;
-	tl.toIndex = (index, vars) => toIndex(index, vars);
-	tl.times = times;
-	tl.progress(1, true).progress(0, true); // pre-render for performance
-	if (config.reversed) {
-	  tl.vars.onReverseComplete();
-	  tl.reverse();
-	}
-	return tl;
-}
